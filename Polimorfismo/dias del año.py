03ee9ae1097baaeb981ed71d4bcfdd488e637ee0
MILEYDI <ls.orejuelag@uea.edu.ec>
2024-06-09 16:52:58 -0500

    TAREA DE PROGRAMACION: HERENCIA, ABSTRACCION, POLIMORFOS, ENCAPSULACION

--- Polimorfismo/dias del año.py
+++ Polimorfismo/dias del año.py
@@ -2,10 +2,10 @@ def novelas(personaje_1_,personaje_2_):
     actor = 0
     while personaje_1_() and personaje_2_():
         print("\nactor",actor)
-        print(">>> Acción de ", actuar, ":", sep="")
-        personaje_1.protagonista(actor_2)
-        print(">>> Acción de ", antagonista_2.nombre, ":", sep="")
-        actor_2.actuar(actor_1)
+        print(">>> Acción de ", "actuar", ":", sep="")
+        personaje_1.protagonista("actor_2")
+        print(">>> Acción de ", "antagonista_2".nombre, ":", sep="")
+        "actor_2".actuar("actor_1")
         actor = actor + 1
     if personaje_1.esta_vivo():
         print("\nha actuado", personaje_1.nombre)
@@ -15,10 +15,10 @@ def novelas(personaje_1_,personaje_2_):
         print("\nexcelente")
 
 
-personaje_1 = protagonista("excelente", 10,4,8,11)
-personaje_2 = antagonista("excelente", 12,5,6,8,)
+personaje_1 = "protagonista"("excelente",)
+personaje_2 = "antagonista"("excelente",)
 
 personaje_1.atributos()
 personaje_2.atributos()
 
-actuan(personaje_1, personaje_2)
+"actuan"(personaje_1, personaje_2)
